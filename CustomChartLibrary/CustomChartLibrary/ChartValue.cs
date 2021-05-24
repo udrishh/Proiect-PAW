996d696479745410bd7446480c574854bd5f02c8
udrishh <udris.bogdan9@gmail.com>
2021-05-24 16:27:17 +0300

    Custom Control refference

---
+++ CustomChartLibrary/CustomChartLibrary/ChartValue.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CustomChartLibrary
+{
+    public class ChartValue
+    {
+        #region Atribute
+        public string Label { set; get; }
+        public int Value { set; get; }
+        #endregion
+
+        #region Constructori
+        public ChartValue(string label, int value)
+        {
+            Label = label;
+            Value = value;
+        }
+        #endregion
+    }
+}
